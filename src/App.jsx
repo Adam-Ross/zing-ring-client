22144933d83b0c171f285b18c9beb5899ea10c3e
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2024-03-20 15:09:37 -0500

    Added super basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  const grabTodos = async () => {
+    try {
+      const res = await fetch(`http://jsonplaceholder.typicode.com/todos`)
+      const data = await res.json()
+      console.log(data)
+      setTodos([...data])
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={grabTodos}>Grab</button>
+      {todos.map((todo) => (
+        <h1>{todo.title}</h1>
+      ))}
     </>
   )
 }
 
 export default App
+
+
+
