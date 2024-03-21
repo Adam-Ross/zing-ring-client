2bc380ddd3ad9b42e6d58cf3e889433369fd479d
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2024-03-20 20:13:36 -0500

    Working in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
 
 function App() {
   const [todos, setTodos] = useState([])
 
   const grabTodos = async () => {
     try {
-      const res = await fetch(`https://jsonplaceholder.typicode.com/todos`)
+      const res = await fetch(`https://zing-ring-server.onrender.com/todos`)
       const data = await res.json()
       console.log(data)
       setTodos([...data])
@@ -21,7 +20,7 @@ function App() {
     <>
       <button onClick={grabTodos}>Grab</button>
       {todos.map((todo) => (
-        <h1>{todo.title}</h1>
+        <h1 key={todo.id}>{todo.body}</h1>
       ))}
     </>
   )
